7d36fcb9560478ec3e6c3a93e4ee627d9f87e88f
CHAMOD RATHNAYAKA <chamodrathnayaka008@gmail.com>
2025-11-13 22:04:21 +0530

    Implement Item management functionality with CRUD operations and UI integration

--- src/main/java/Controller/Item/ItemService.java
+++ src/main/java/Controller/Item/ItemService.java
@@ -1,4 +1,15 @@
 package Controller.Item;
 
+import Model.DTO.ItemInfoDTO;
+import javafx.collections.ObservableList;
+
 public interface ItemService {
+
+    void addItemInfo(String itemCode,String description,String category,int qtyOnHand,double unitPrice);
+
+    void updateItemInfo(String itemCode,String description,String category,int qtyOnHand,double unitPrice);
+
+    void deleteItemInfo(String itemCode);
+
+    ObservableList<ItemInfoDTO> loadItemTable();
 }
--- src/main/java/Model/DTO/ItemInfoDTO.java
+++ src/main/java/Model/DTO/ItemInfoDTO.java
@@ -1,4 +1,18 @@
 package Model.DTO;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class ItemInfoDTO {
+
+    private String itemCode;
+    private String description;
+    private String category;
+    private int qtyOnHand;
+    private double unitPrice;
+
 }
