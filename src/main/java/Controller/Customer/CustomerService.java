726cf2eb003602d50ce489c9fcada315ab90096c
CHAMOD RATHNAYAKA <chamodrathnayaka008@gmail.com>
2025-11-13 21:48:26 +0530

    Add CustomerInfoDTO and CustomerService interface with CRUD methods

--- src/main/java/Controller/Customer/CustomerService.java
+++ src/main/java/Controller/Customer/CustomerService.java
@@ -1,4 +1,17 @@
 package Controller.Customer;
 
+import Model.DTO.CustomerInfoDTO;
+import javafx.collections.ObservableList;
+
 public interface CustomerService {
+
+    void addCustomerDetails(String custId, String title, String name, String dob, double salary, String address, String city, String province, String postalCode);
+
+    void deleteCustomerDetails(String custId);
+
+    void updateCustomerDetails(String custId, String title, String name, String dob, double salary, String address, String city, String province, String postalCode);
+
+    ObservableList<CustomerInfoDTO> loadCustomerDetails();
+
+
 }
--- src/main/java/Model/DTO/CustomerInfoDTO.java
+++ src/main/java/Model/DTO/CustomerInfoDTO.java
@@ -1,4 +1,22 @@
 package Model.DTO;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class CustomerInfoDTO {
+
+    private String custId;
+    private String title;
+    private String name;
+    private String dob;
+    private double salary;
+    private String address;
+    private String city;
+    private String province;
+    private String postalCode;
+
 }
