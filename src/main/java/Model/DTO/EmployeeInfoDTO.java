523edaa4320eb340a6809182260d36ccf1c2cc33
CHAMOD RATHNAYAKA <chamodrathnayaka008@gmail.com>
2025-11-13 22:03:51 +0530

    Add EmployeeInfoDTO and EmployeeService interface with CRUD methods

--- src/main/java/Model/DTO/EmployeeInfoDTO.java
+++ src/main/java/Model/DTO/EmployeeInfoDTO.java
@@ -1,4 +1,23 @@
 package Model.DTO;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class EmployeeInfoDTO {
+
+    private String employeeId;
+    private String name;
+    private String nic;
+    private String dob;
+    private String position;
+    private double salary;
+    private String contactNumber;
+    private String address;
+    private String joinedDate;
+    private String status;
+
 }
