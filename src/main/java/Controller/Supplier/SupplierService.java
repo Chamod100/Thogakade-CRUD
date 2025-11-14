0c9e7ab56fc22fff7132b0577d00759aaa6fed06
CHAMOD RATHNAYAKA <chamodrathnayaka008@gmail.com>
2025-11-14 22:51:57 +0530

    Add methods for supplier information management in SupplierService interface

--- src/main/java/Controller/Supplier/SupplierService.java
+++ src/main/java/Controller/Supplier/SupplierService.java
@@ -1,4 +1,16 @@
 package Controller.Supplier;
 
+import Model.DTO.SupplierInfoDTO;
+import javafx.collections.ObservableList;
+
 public interface SupplierService {
+
+        void addSupplierInfo(String supplierId,String name,String companyName,String address,String city,String province,String postalCode,String phone,String email);
+
+        void deleteSuppierInfo(String supplierId);
+
+        void updateSupplierInfo(String supplierId,String name,String companyName,String address,String city,String province,String postalCode,String phone,String email);
+
+        ObservableList<SupplierInfoDTO> loadSupplierTable();
 }
+
